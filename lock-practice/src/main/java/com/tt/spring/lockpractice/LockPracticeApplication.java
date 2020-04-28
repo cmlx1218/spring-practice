310d862bca8612884ee7fb89595fefd053f564e5
cmlx <cmlx1218@163.com>
2020-04-28 19:02:09 +0800

    .feat:初始化锁相关

---
+++ lock-practice/src/main/java/com/tt/spring/lockpractice/LockPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.tt.spring.lockpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockPracticeApplication.class, args);
+    }
+
+}
